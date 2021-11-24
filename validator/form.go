ce8114b6903b73012e28e043968a2514932d2008
leetaifook <lee@leetaifook.com>
2021-11-24 10:17:12 +0800

    fix point bug

--- validator/form.go
+++ validator/form.go
@@ -1,15 +1,28 @@
 package validator
 
 import (
+	"context"
 	"net/url"
 )
 
+//必须传入指针
 func FormStruct(v interface{}, values url.Values) (err error) {
-	if err = Decoder.Decode(&v, values); err != nil {
+	if err = Decoder.Decode(v, values); err != nil {
 		return
 	}
 
 	err = Validate.Struct(v)
+
+	return
+}
+
+//必须传入指针
+func FormStructCtx(ctx context.Context, v interface{}, values url.Values) (err error) {
+	if err = Decoder.Decode(v, values); err != nil {
+		return
+	}
+
+	err = Validate.StructCtx(ctx, v)
 
 	return
 }
--- validator/json.go
+++ validator/json.go
@@ -1,17 +1,36 @@
 package validator
 
-import "encoding/json"
+import (
+	"context"
+	"encoding/json"
+)
 
+//必须传入指针
 func Json(v interface{}, jsonRaw string) (err error) {
 	if err = Validate.Var(jsonRaw, "required,json"); err != nil {
 		return
 	}
 
-	if err = json.Unmarshal([]byte(jsonRaw), &v); err != nil {
+	if err = json.Unmarshal([]byte(jsonRaw), v); err != nil {
 		return
 	}
 
 	err = Validate.Struct(v)
+
+	return
+}
+
+//必须传入指针
+func JsonCtx(ctx context.Context, v interface{}, jsonRaw string) (err error) {
+	if err = Validate.VarCtx(ctx, jsonRaw, "required,json"); err != nil {
+		return
+	}
+
+	if err = json.Unmarshal([]byte(jsonRaw), v); err != nil {
+		return
+	}
+
+	err = Validate.StructCtx(ctx, v)
 
 	return
 }
