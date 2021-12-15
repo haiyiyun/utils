dae449a5f7b530049815722003229039d5cac4c3
leetaifook <lee@leetaifook.com>
2021-12-15 10:18:22 +0800

    add time

--- validator/decoder_custom_type_func.go
+++ validator/decoder_custom_type_func.go
@@ -23,6 +23,25 @@ func init() {
 	}, help.DateTime{})
 
 	Decoder.RegisterCustomTypeFunc(func(vals []string) (interface{}, error) {
+		dateLayout := "2006-01-02"
+		dateTimeLayout := "2006-01-02 15:04:05"
+		zoneLayout := "Z07:00"
+		localZone := time.Now().Format(zoneLayout)
+		timeStr := vals[0]
+
+		lenTimeStr := len(timeStr)
+		if len(dateLayout) == lenTimeStr {
+			timeStr = timeStr + "T00:00:00" + localZone
+		} else if len(dateTimeLayout) == lenTimeStr {
+			timeByte := []byte(timeStr)
+			timeByte[10] = 'T'
+			timeStr = string(timeByte) + localZone
+		}
+
+		return time.Parse(time.RFC3339, timeStr)
+	}, time.Time{})
+
+	Decoder.RegisterCustomTypeFunc(func(vals []string) (interface{}, error) {
 		return primitive.ObjectIDFromHex(vals[0])
 	}, primitive.ObjectID{})
 
