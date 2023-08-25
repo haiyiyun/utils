78f322896a0764a7f7836e1a61dc49fab270b863
leetaifook <lee@leetaifook.com>
2023-08-25 12:16:04 +0800

    Updata client

--- go.mod
+++ go.mod
@@ -4,18 +4,18 @@ go 1.17
 
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible
-	github.com/haiyiyun/log v0.0.0-20211115100502-be01af77681c
-	github.com/haiyiyun/mongodb v0.0.0-20211220082059-4679b4d7ff22
-	github.com/haiyiyun/plugins/user v0.0.0-20211220083352-24d3fca02cf0
+	github.com/haiyiyun/log v0.0.0-20220125040429-3699970552a6
+	github.com/haiyiyun/mongodb v0.0.0-20220108162216-33ee0dca6d1c
+	github.com/haiyiyun/plugins/user v0.0.0-20230120122813-2f699304561d
 	github.com/haiyiyun/uuid v0.0.0-20211115101403-e9c2d7112f99
 	github.com/haiyiyun/validator v1.1.0
 	github.com/robfig/cron/v3 v3.0.1
-	go.mongodb.org/mongo-driver v1.8.1
+	go.mongodb.org/mongo-driver v1.12.1
 )
 
 require (
 	github.com/leodido/go-urn v1.2.1 // indirect
-	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871 // indirect
-	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/text v0.7.0 // indirect
 )
--- http/client/client.go
+++ http/client/client.go
@@ -36,31 +36,105 @@ func TimeoutDialer(cTimeout, rwTimeout time.Duration) func(netw, addr string) (c
 	}
 }
 
-func SkipVerifyAndTimeOutTransport(insecureSkipVerify bool, headerTimeout, connectTimeout, readWriteTimeout time.Duration) *http.Transport {
-	return &http.Transport{
-		Proxy: http.ProxyFromEnvironment,
-		Dial:  TimeoutDialer(connectTimeout, readWriteTimeout),
-		TLSClientConfig: &tls.Config{
-			InsecureSkipVerify: insecureSkipVerify,
-		},
-		ResponseHeaderTimeout: headerTimeout,
-	}
-}
-
-func NewClient() *Client {
+func NewClientWithTransport(transport *http.Transport) *Client {
 	c := &Client{Client: &http.Client{}}
-	c.Transport = SkipVerifyAndTimeOutTransport(true, 60*time.Second, 60*time.Second, 2*time.Minute)
+	c.Transport = transport
 	return c
 }
 
-func NewClientWithHeader(header http.Header) *Client {
+func NewClientWhthTransportAndHeader(transport *http.Transport, header http.Header) *Client {
 	c := &Client{
 		Client: &http.Client{},
 		header: header,
 	}
 
-	c.Transport = SkipVerifyAndTimeOutTransport(true, 60*time.Second, 60*time.Second, 2*time.Minute)
+	c.Transport = transport
 	return c
+}
+
+func TransportWithProxyAndTimeOut(proxy func(*http.Request) (*url.URL, error), tlsClientConfig *tls.Config, headerTimeout, connectTimeout, readWriteTimeout time.Duration) *http.Transport {
+	return &http.Transport{
+		Proxy:                 proxy,
+		Dial:                  TimeoutDialer(connectTimeout, readWriteTimeout),
+		TLSClientConfig:       tlsClientConfig,
+		ResponseHeaderTimeout: headerTimeout,
+	}
+}
+
+func TransportWithTimeOut(tlsClientConfig *tls.Config, headerTimeout, connectTimeout, readWriteTimeout time.Duration) *http.Transport {
+	return TransportWithProxyAndTimeOut(http.ProxyFromEnvironment, tlsClientConfig, headerTimeout, connectTimeout, readWriteTimeout)
+}
+
+func TransportWithProxyAndSkipVerifyAndTimeOut(proxy func(*http.Request) (*url.URL, error), headerTimeout, connectTimeout, readWriteTimeout time.Duration) *http.Transport {
+	tlsClientConfig := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	return TransportWithProxyAndTimeOut(proxy, tlsClientConfig, headerTimeout, connectTimeout, readWriteTimeout)
+}
+
+func TransportWithSkipVerifyAndTimeOut(headerTimeout, connectTimeout, readWriteTimeout time.Duration) *http.Transport {
+	tlsClientConfig := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	return TransportWithTimeOut(tlsClientConfig, headerTimeout, connectTimeout, readWriteTimeout)
+}
+
+func DefaultTransport() *http.Transport {
+	return TransportWithSkipVerifyAndTimeOut(60*time.Second, 60*time.Second, 2*time.Minute)
+}
+
+func DefaultTransportWithProxy(proxy func(*http.Request) (*url.URL, error)) *http.Transport {
+	return TransportWithProxyAndSkipVerifyAndTimeOut(proxy, 60*time.Second, 60*time.Second, 2*time.Minute)
+}
+
+func DefaultTransportWithTLS(tlsClientConfig *tls.Config) *http.Transport {
+	return TransportWithTimeOut(tlsClientConfig, 60*time.Second, 60*time.Second, 2*time.Minute)
+}
+
+func DefaultTransportWithTLSAndProxy(proxy func(*http.Request) (*url.URL, error), tlsClientConfig *tls.Config) *http.Transport {
+	return TransportWithProxyAndTimeOut(proxy, tlsClientConfig, 60*time.Second, 60*time.Second, 2*time.Minute)
+}
+
+func NewClient() *Client {
+	transport := DefaultTransport()
+	return NewClientWithTransport(transport)
+}
+
+func NewClientWithHeader(header http.Header) *Client {
+	transport := DefaultTransport()
+	return NewClientWhthTransportAndHeader(transport, header)
+}
+
+func NewClientWithProxy(proxy func(*http.Request) (*url.URL, error)) *Client {
+	transport := DefaultTransportWithProxy(proxy)
+	return NewClientWithTransport(transport)
+}
+
+func NewClientWithProxyAndHeader(proxy func(*http.Request) (*url.URL, error), header http.Header) *Client {
+	transport := DefaultTransportWithProxy(proxy)
+	return NewClientWhthTransportAndHeader(transport, header)
+}
+
+func NewClientWithTLS(tlsClientConfig *tls.Config) *Client {
+	transport := DefaultTransportWithTLS(tlsClientConfig)
+	return NewClientWithTransport(transport)
+}
+
+func NewClientWhthTLSAndHeader(tlsClientConfig *tls.Config, header http.Header) *Client {
+	transport := DefaultTransportWithTLS(tlsClientConfig)
+	return NewClientWhthTransportAndHeader(transport, header)
+}
+
+func NewClientWithTLSAndProxy(tlsClientConfig *tls.Config, proxy func(*http.Request) (*url.URL, error)) *Client {
+	transport := DefaultTransportWithTLSAndProxy(proxy, tlsClientConfig)
+	return NewClientWithTransport(transport)
+}
+
+func NewClientWhthTLSAndProxyAndHeader(tlsClientConfig *tls.Config, proxy func(*http.Request) (*url.URL, error), header http.Header) *Client {
+	transport := DefaultTransportWithTLSAndProxy(proxy, tlsClientConfig)
+	return NewClientWhthTransportAndHeader(transport, header)
 }
 
 func (c *Client) Gzip() *Client {
@@ -74,12 +148,6 @@ func (c *Client) GzipDisable() *Client {
 }
 
 func (c *Client) doMethod(method, rawurl string, header http.Header, body io.Reader) (*http.Response, error) {
-	req, err := http.NewRequest(method, rawurl, body)
-	if err != nil {
-		log.Error("NewRequest error:", err)
-		return nil, err
-	}
-
 	if header == nil {
 		header = http.Header{}
 	}
@@ -92,8 +160,28 @@ func (c *Client) doMethod(method, rawurl string, header http.Header, body io.Rea
 		}
 	}
 
+	var ir io.Reader
+	bu := new(bytes.Buffer)
+	if _, err := bu.ReadFrom(body); err != nil {
+		return nil, err
+	}
+
 	if c.useGzip {
 		header.Add("Content-Encoding", "gzip")
+
+		bufw := new(bytes.Buffer)
+		gzw := gzip.NewWriter(bufw)
+		gzw.Write(bu.Bytes())
+		gzw.Close()
+		ir = bufw
+	} else {
+		ir = bu
+	}
+
+	req, err := http.NewRequest(method, rawurl, ir)
+	if err != nil {
+		log.Error("NewRequest error:", err)
+		return nil, err
 	}
 
 	if len(header) > 0 {
@@ -104,8 +192,8 @@ func (c *Client) doMethod(method, rawurl string, header http.Header, body io.Rea
 	return resp, respErr
 }
 
-func (c *Client) get(rawurl string, header http.Header, body io.Reader) (*http.Response, error) {
-	return c.doMethod("GET", rawurl, header, body)
+func (c *Client) get(rawurl string, header http.Header, data io.Reader) (*http.Response, error) {
+	return c.doMethod("GET", rawurl, header, data)
 }
 
 func (c *Client) Get(rawurl string) (*http.Response, error) {
@@ -116,102 +204,74 @@ func (c *Client) GetWithHeader(rawurl string, header http.Header) (*http.Respons
 	return c.get(rawurl, header, nil)
 }
 
-func (c *Client) GetForm(rawurl string, data url.Values) (*http.Response, error) {
-	var ir io.Reader
-	if c.useGzip {
-		bufw := new(bytes.Buffer)
-		gzw := gzip.NewWriter(bufw)
-		gzw.Write([]byte(data.Encode()))
-		gzw.Close()
-		ir = bufw
-	} else {
-		ir = strings.NewReader(data.Encode())
-	}
-
-	header := http.Header{}
-	header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-	return c.get(rawurl, header, ir)
-}
-
 func (c *Client) GetFormWithHeader(rawurl string, header http.Header, data url.Values) (*http.Response, error) {
-	var ir io.Reader
-	if c.useGzip {
-		bufw := new(bytes.Buffer)
-		gzw := gzip.NewWriter(bufw)
-		gzw.Write([]byte(data.Encode()))
-		gzw.Close()
-		ir = bufw
-	} else {
-		ir = strings.NewReader(data.Encode())
-	}
-
 	header.Add("Content-Type", "application/x-www-form-urlencoded")
 
-	return c.get(rawurl, header, ir)
+	return c.get(rawurl, header, strings.NewReader(data.Encode()))
 }
 
-func (c *Client) PostWithHeader(rawurl string, header http.Header, body io.Reader) (*http.Response, error) {
-	return c.doMethod("POST", rawurl, header, body)
+func (c *Client) GetForm(rawurl string, data url.Values) (*http.Response, error) {
+	return c.GetFormWithHeader(rawurl, http.Header{}, data)
 }
 
-func (c *Client) Post(rawurl string, body io.Reader) (*http.Response, error) {
-	return c.PostWithHeader(rawurl, nil, body)
+func (c *Client) GetJsonWithHeader(rawurl string, header http.Header, data []byte) (*http.Response, error) {
+	header.Add("Content-Type", "application/json")
+
+	return c.get(rawurl, header, bytes.NewReader(data))
 }
 
-func (c *Client) PostForm(rawurl string, data url.Values) (*http.Response, error) {
-	var ir io.Reader
-	if c.useGzip {
-		bufw := new(bytes.Buffer)
-		gzw := gzip.NewWriter(bufw)
-		gzw.Write([]byte(data.Encode()))
-		gzw.Close()
-		ir = bufw
-	} else {
-		ir = strings.NewReader(data.Encode())
-	}
+func (c *Client) GetJson(rawurl string, data []byte) (*http.Response, error) {
+	return c.GetJsonWithHeader(rawurl, http.Header{}, data)
+}
 
-	header := http.Header{}
-	header.Set("Content-Type", "application/x-www-form-urlencoded")
+func (c *Client) GetFormJsonWithHeader(rawurl string, header http.Header, data []byte) (*http.Response, error) {
+	header.Add("Content-Type", "application/x-www-form-urlencoded")
+	header.Add("Content-Type", "application/json")
 
-	return c.PostWithHeader(rawurl, header, ir)
+	return c.get(rawurl, header, bytes.NewReader(data))
+}
+
+func (c *Client) GetFormJson(rawurl string, data []byte) (*http.Response, error) {
+	return c.GetFormJsonWithHeader(rawurl, http.Header{}, data)
+}
+
+func (c *Client) PostWithHeader(rawurl string, header http.Header, data io.Reader) (*http.Response, error) {
+	return c.doMethod("POST", rawurl, header, data)
+}
+
+func (c *Client) Post(rawurl string, data io.Reader) (*http.Response, error) {
+	return c.PostWithHeader(rawurl, nil, data)
 }
 
 func (c *Client) PostFormWithHeader(rawurl string, header http.Header, data url.Values) (*http.Response, error) {
-	var ir io.Reader
-	if c.useGzip {
-		bufw := new(bytes.Buffer)
-		gzw := gzip.NewWriter(bufw)
-		gzw.Write([]byte(data.Encode()))
-		gzw.Close()
-		ir = bufw
-	} else {
-		ir = strings.NewReader(data.Encode())
-	}
-
 	header.Add("Content-Type", "application/x-www-form-urlencoded")
 
-	return c.PostWithHeader(rawurl, header, ir)
+	return c.PostWithHeader(rawurl, header, strings.NewReader(data.Encode()))
 }
 
-func (c *Client) PostMultipart(rawurl string, data map[string][]string) (resp *http.Response, err error) {
-	body, ct, err := multipart.Open(data)
-	if err != nil {
-		return
-	}
+func (c *Client) PostForm(rawurl string, data url.Values) (*http.Response, error) {
+	return c.PostFormWithHeader(rawurl, http.Header{}, data)
+}
 
-	if c.useGzip {
-		bufw := new(bytes.Buffer)
-		gzw := gzip.NewWriter(bufw)
-		gzw.Write(body.Bytes())
-		gzw.Close()
-		body = bufw
-	}
+func (c *Client) PostJsonWithHeader(rawurl string, header http.Header, data []byte) (*http.Response, error) {
+	header.Add("Content-Type", "application/json")
 
-	header := http.Header{}
-	header.Set("Content-Type", ct)
+	return c.PostWithHeader(rawurl, header, bytes.NewReader(data))
+}
 
-	return c.PostWithHeader(rawurl, header, body)
+func (c *Client) PostJson(rawurl string, data []byte) (*http.Response, error) {
+	return c.PostJsonWithHeader(rawurl, http.Header{}, data)
+}
+
+func (c *Client) PostFormJsonWithHeader(rawurl string, header http.Header, data []byte) (*http.Response, error) {
+	header.Add("Content-Type", "application/x-www-form-urlencoded")
+	header.Add("Content-Type", "application/json")
+
+	return c.PostWithHeader(rawurl, header, bytes.NewReader(data))
+}
+
+func (c *Client) PostFormJson(rawurl string, data []byte) (*http.Response, error) {
+	return c.PostFormJsonWithHeader(rawurl, http.Header{}, data)
 }
 
 func (c *Client) PostMultipartWithHeader(rawurl string, header http.Header, data map[string][]string) (resp *http.Response, err error) {
@@ -220,64 +280,54 @@ func (c *Client) PostMultipartWithHeader(rawurl string, header http.Header, data
 		return
 	}
 
-	if c.useGzip {
-		bufw := new(bytes.Buffer)
-		gzw := gzip.NewWriter(bufw)
-		gzw.Write(body.Bytes())
-		gzw.Close()
-		body = bufw
-	}
-
 	header.Add("Content-Type", ct)
 
 	return c.PostWithHeader(rawurl, header, body)
 }
 
-func (c *Client) PutWithHeader(rawurl string, header http.Header, body io.Reader) (*http.Response, error) {
-	return c.doMethod("PUT", rawurl, header, body)
+func (c *Client) PostMultipart(rawurl string, data map[string][]string) (resp *http.Response, err error) {
+	return c.PostMultipartWithHeader(rawurl, http.Header{}, data)
 }
 
-func (c *Client) Put(rawurl string, body io.Reader) (*http.Response, error) {
-	return c.PutWithHeader(rawurl, nil, body)
+func (c *Client) PutWithHeader(rawurl string, header http.Header, data io.Reader) (*http.Response, error) {
+	return c.doMethod("PUT", rawurl, header, data)
 }
 
-func (c *Client) PutForm(rawurl string, data url.Values) (*http.Response, error) {
-	var ir io.Reader
-	if c.useGzip {
-		bufw := new(bytes.Buffer)
-		gzw := gzip.NewWriter(bufw)
-		gzw.Write([]byte(data.Encode()))
-		gzw.Close()
-		ir = bufw
-	} else {
-		ir = strings.NewReader(data.Encode())
-	}
-
-	header := http.Header{}
-	header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-	return c.PutWithHeader(rawurl, header, ir)
+func (c *Client) Put(rawurl string, data io.Reader) (*http.Response, error) {
+	return c.PutWithHeader(rawurl, nil, data)
 }
 
 func (c *Client) PutFormWithHeader(rawurl string, header http.Header, data url.Values) (*http.Response, error) {
-	var ir io.Reader
-	if c.useGzip {
-		bufw := new(bytes.Buffer)
-		gzw := gzip.NewWriter(bufw)
-		gzw.Write([]byte(data.Encode()))
-		gzw.Close()
-		ir = bufw
-	} else {
-		ir = strings.NewReader(data.Encode())
-	}
-
 	header.Add("Content-Type", "application/x-www-form-urlencoded")
 
-	return c.PutWithHeader(rawurl, header, ir)
+	return c.PutWithHeader(rawurl, header, strings.NewReader(data.Encode()))
 }
 
-func (c *Client) delete(rawurl string, header http.Header, body io.Reader) (*http.Response, error) {
-	return c.doMethod("DELETE", rawurl, header, body)
+func (c *Client) PutForm(rawurl string, data url.Values) (*http.Response, error) {
+	return c.PutFormWithHeader(rawurl, http.Header{}, data)
+}
+
+func (c *Client) PutJsonWithHeader(rawurl string, header http.Header, data []byte) (*http.Response, error) {
+	header.Add("Content-Type", "application/json")
+	return c.PutWithHeader(rawurl, header, bytes.NewReader(data))
+}
+
+func (c *Client) PutJson(rawurl string, data []byte) (*http.Response, error) {
+	return c.PutJsonWithHeader(rawurl, http.Header{}, data)
+}
+
+func (c *Client) PutFormJsonWithHeader(rawurl string, header http.Header, data []byte) (*http.Response, error) {
+	header.Add("Content-Type", "application/x-www-form-urlencoded")
+	header.Add("Content-Type", "application/json")
+	return c.PutWithHeader(rawurl, header, bytes.NewReader(data))
+}
+
+func (c *Client) PutFormJson(rawurl string, data []byte) (*http.Response, error) {
+	return c.PutFormJsonWithHeader(rawurl, http.Header{}, data)
+}
+
+func (c *Client) delete(rawurl string, header http.Header, data io.Reader) (*http.Response, error) {
+	return c.doMethod("DELETE", rawurl, header, data)
 }
 
 func (c *Client) Delete(rawurl string) (*http.Response, error) {
@@ -288,37 +338,30 @@ func (c *Client) DeleteWithHeader(rawurl string, header http.Header) (*http.Resp
 	return c.delete(rawurl, header, nil)
 }
 
-func (c *Client) DeleteForm(rawurl string, data url.Values) (*http.Response, error) {
-	var ir io.Reader
-	if c.useGzip {
-		bufw := new(bytes.Buffer)
-		gzw := gzip.NewWriter(bufw)
-		gzw.Write([]byte(data.Encode()))
-		gzw.Close()
-		ir = bufw
-	} else {
-		ir = strings.NewReader(data.Encode())
-	}
-
-	header := http.Header{}
-	header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-	return c.delete(rawurl, header, ir)
+func (c *Client) DeleteFormWithHeader(rawurl string, header http.Header, data url.Values) (*http.Response, error) {
+	header.Add("Content-Type", "application/x-www-form-urlencoded")
+	return c.delete(rawurl, header, strings.NewReader(data.Encode()))
 }
 
-func (c *Client) DeleteFormWithHeader(rawurl string, header http.Header, data url.Values) (*http.Response, error) {
-	var ir io.Reader
-	if c.useGzip {
-		bufw := new(bytes.Buffer)
-		gzw := gzip.NewWriter(bufw)
-		gzw.Write([]byte(data.Encode()))
-		gzw.Close()
-		ir = bufw
-	} else {
-		ir = strings.NewReader(data.Encode())
-	}
+func (c *Client) DeleteForm(rawurl string, data url.Values) (*http.Response, error) {
+	return c.DeleteFormWithHeader(rawurl, http.Header{}, data)
+}
 
+func (c *Client) DeleteJsonWithHeader(rawurl string, header http.Header, data []byte) (*http.Response, error) {
+	header.Add("Content-Type", "application/json")
+	return c.delete(rawurl, header, bytes.NewReader(data))
+}
+
+func (c *Client) DeleteJson(rawurl string, data []byte) (*http.Response, error) {
+	return c.DeleteJsonWithHeader(rawurl, http.Header{}, data)
+}
+
+func (c *Client) DeleteFormJsonWithHeader(rawurl string, header http.Header, data []byte) (*http.Response, error) {
 	header.Add("Content-Type", "application/x-www-form-urlencoded")
+	header.Add("Content-Type", "application/json")
+	return c.delete(rawurl, header, bytes.NewReader(data))
+}
 
-	return c.delete(rawurl, header, ir)
+func (c *Client) DeleteFormJson(rawurl string, data []byte) (*http.Response, error) {
+	return c.DeleteFormJsonWithHeader(rawurl, http.Header{}, data)
 }
